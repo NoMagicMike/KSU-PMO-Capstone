18d511cf4538262c465d13c77d357ec728167552
NoMagicMike <54690744+NoMagicMike@users.noreply.github.com>
2020-03-10 18:16:09 -0400

    Added Admin registration boolean

--- register.php
+++ register.php
@@ -6,6 +6,7 @@ include 'navbar.php';
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
+$user_admin = 0;
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -61,20 +62,27 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $confirm_password_err = "Password did not match.";
         }
     }
+    // Check admin priveledge
+    if (isset($_POST["adminCheckBox"])){
+      $user_admin = (int)$_POST["adminCheckBox"];
+    }
+
+
 
     // Check input errors before inserting in database
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
 
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username, password, user_admin) VALUES (?, ?, ?)";
 
         if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            // Bind variables to the prepared statement as parameters (Number of "sss" is equal to number of parameters)
+            mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_password, $param_user_admin);
 
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_user_admin = $user_admin;
 
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
@@ -116,6 +124,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <label>Confirm Password</label>
                 <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-control">
+                <label>Allow Admin Priveledges?</label>
+                <input type="checkbox" name="adminCheckBox" value="1">
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
