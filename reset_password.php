6fa0240a03d1377b5e935984153cded88139ee79
NoMagicMike <54690744+NoMagicMike@users.noreply.github.com>
2020-03-23 19:23:39 -0400

    Reset password works with header bug

--- reset_password.php
+++ reset_password.php
@@ -42,33 +42,34 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Check input errors before updating the database
     if(empty($new_password_err) && empty($confirm_password_err)){
         // Prepare an update statement
-        $sql = "UPDATE User SET password = ? WHERE user_id = ?";
+        $sql = "UPDATE user SET password = :password WHERE user_id = :user_id";
         
-        if($stmt = mysqli_prepare($link, $sql)){
+        if($stmt = $pdo->prepare($sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
+            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+            $stmt->bindParam(":user_id", $param_id, PDO::PARAM_INT);
             
             // Set parameters
             $param_password = password_hash($new_password, PASSWORD_DEFAULT);
             $param_id = $_SESSION["user_id"];
             
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if($stmt->execute()){
                 // Password updated successfully. Destroy the session, and redirect to login page
                 session_destroy();
-                header("location: login.php");
+                header("Location: login.php");
                 exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
             // Close statement
-            mysqli_stmt_close($stmt);
+            unset($stmt);
         }
     }
     
     // Close connection
-    mysqli_close($link);
+    unset($pdo);
 }
 ?>
  
@@ -77,7 +78,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
     <div class="jumbotron">
         <h2>Reset Password(Does not work yet!)</h2>
-        <p>Please fill out this form to reset your password.</p>
+        <?php echo "<p> Hello, {$_SESSION['username']}! Please fill out this form to reset your password.</p>"?>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
             <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
                 <label>New Password</label>
