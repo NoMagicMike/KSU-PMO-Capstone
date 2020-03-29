8306e53af03722d78c0b81c701edd257c991be57
NoMagicMike <54690744+NoMagicMike@users.noreply.github.com>
2020-03-29 16:21:04 -0400

    Update User Active. Able to update username and admin status.

--- update_user.php
+++ update_user.php
@@ -12,10 +12,16 @@ if (isset($_GET['user_id'])) {
     if (!empty($_POST)) {
         // This part is similar to the create_user.php, but instead we update a record instead of creating one
         $username = isset($_POST["username"]) ? $_POST["username"] : '';
-        $user_admin = isset($_POST["user_admin"]) ? $_POST["user_admin"] : '';
+        if (isset($_POST["user_adminBox"])){
+          $user_admin = (int)$_POST["user_adminBox"];
+        }
+        else {
+          $user_admin = (int)$_POST[0];
+        } 
         // Update the record with prepared sql
         $stmt = $conn->prepare('UPDATE user SET username = ?, user_admin = ? WHERE user_id = ?');
-        $stmt->execute([$username, $user_admin, $_GET['user_id']]);
+        $stmt->execute([$username,$user_admin, $_GET['user_id']]);
+        
         //javascript to pop up a message so the user can acknowledge an alert that the record was updated.
         //URL below will need to be changed to one tht will work with the actual server!
       	echo "<script type='text/JavaScript'>
@@ -38,14 +44,16 @@ if (isset($_GET['user_id'])) {
 <?=template_header('Update User')?>
 <!--Start of container for Update User section-->
 <div class="container">
-  <!--In the heading, pull up the ID number and Title of the user selected for updating-->
+  <!--In the heading, pull up the ID number and username of the user selected for updating-->
 	<h2>Update User #<?=$user['user_id']?> - <?=$user['username']?></h2>
     <!--Link this form's actions to this file, update_user.php-->
     <form action="update_user.php?user_id=<?=$user['user_id']?>" method="post">
         <label for="username">Username :</label>
         <input type="text" name="username" value="<?=$user['username']?>" id="username">
         <br />
-        <label for="user_admin">Admin? :</label>
+        <label>Allow Admin Priveledges?</label>
+        <input type="checkbox" name="user_adminBox" value="1">
+        </div>
         <br />
         <input type="submit" value="Update">
         <br />
