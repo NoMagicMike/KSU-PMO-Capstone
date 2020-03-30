5d7ae2d7e2e6b74b1c5278f2a7b3bfccaeb5d244
NoMagicMike <54690744+NoMagicMike@users.noreply.github.com>
2020-03-29 20:49:31 -0400

    New Navbar with working mobile version (bug: Nav Not Checking Session)

--- navbar.php
+++ navbar.php
@@ -7,72 +7,76 @@
 	<!-- Bootstrap References -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	</head>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-	        <ul class="navbar-nav mr-auto">
-	          <?php if(isset($_SESSION['loggedin'])): ?>
-	            <li class="nav-item active text-nowrap">
-	                <a class="nav-link" href="index.php"><i class="fas fa-home"></i> Home<span class="sr-only">(current)</span></a>
-	            </li>
-	            <li class="nav-item dropdown">
-	              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-	                Projects
-	              </a>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-	              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-	                <a class="dropdown-item" href="create_project.php">Add New Project</a>
-	                <a class="dropdown-item" href="get_project.php">View All Projects</a>
-	                <div class="dropdown-divider"></div>
-	              </div>
-	    				</li>
-	    				<?php if($_SESSION['adminCheck'] == 1): ?>
-	    					<li class="nav-item dropdown">
-	    	          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-	    	            Admin Portal
-	    	          </a>
+      <div class="collapse navbar-collapse justify-content-between" id="navbarsExample04">
+				<a class="navbar-brand d-md-none" href="index.php"><img src="static/KSUBrandLong.png" height="50" alt="KSU Logo"></a>
+				
+        <ul class="navbar-nav">
+					<?php if(isset($_SESSION['loggedin'])): ?>
+						<li class="nav-item active text-nowrap">
+								<a class="nav-link" href="index.php"><i class="fas fa-home"></i> Home<span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								Projects
+							</a>
 
-	    	          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-	    	            <a class="dropdown-item" href="add_user.php">Add New Users</a>
-	    	            <a class="dropdown-item" href="get_user.php">View All Users</a>
-	    	            <div class="dropdown-divider"></div>
-	    	          </div>
-	    					</li>
-	    				<?php endif; ?>
-	        </ul>
-	    </div>
-	    <div class="mx-auto order-0">
-	        <a class="navbar-brand mx-auto" href="index.php"><img src="static/KSUBrandLong.png" height="50" alt="KSU Logo"></a>
-	        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	        <span class="navbar-toggler-icon"></span>
-	        </button>
-	    </div>
-	    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-	        <ul class="navbar-nav ml-auto text-nowrap">
-	          <form class="form-inline my-2 my-lg-0 ">
-	            <input class="form-control mr-sm-2" type="search" placeholder="Not Yet Functional :(" aria-label="Search">
-	          	<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-	          </form>
-	        <?php else: ?>
-	            <!-- If logged out, show nothing in Navbar -->
-	        <?php endif; ?>
-	            <li class="nav-item">
-	              <?php if(isset( $_SESSION['loggedin'])): ?>
-	                <li class="nav-item dropdown">
-	                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-	                    Account
-	                  </a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item" href="create_project.php">Add New Project</a>
+								<a class="dropdown-item" href="get_project.php">View All Projects</a>
+								<div class="dropdown-divider"></div>
+							</div>
+						</li>
+						<?php endif; ?>
+						<?php if($_SESSION['adminCheck'] == 1): ?>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Admin Portal
+								</a>
 
-	                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-	                    <a class="dropdown-item" href="logout.php">Logout</a>
-											<a class="dropdown-item" href="reset_password.php">Reset Password</a>
-	                  </div>
-	                </li>
+								<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+									<a class="dropdown-item" href="add_user.php">Add New Users</a>
+									<a class="dropdown-item" href="get_user.php">View All Users</a>
+									<div class="dropdown-divider"></div>
+								</div>
+							</li>
+					<?php endif; ?>
+					</ul>	
+					
+					
+					<a class="navbar-brand d-none d-md-block" href="index.php"><img src="static/KSUBrandLong.png" height="50" alt="KSU Logo"></a>
+			    
+					
+					<ul class="navbar-nav">
+						<form class="form-inline my-2 my-md-0">
+							<input class="form-control mr-sm-2" type="search" placeholder="Not Yet Functional :(" aria-label="Search">
+							<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+						</form>
+						<li class="nav-item">
+							<?php if(isset( $_SESSION['loggedin'])): ?>
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										Account
+									</a>
 
-	              <?php else: ?>
-	                  <a class="nav-link text-right" href="login.php">Login <span class="sr-only">(current)</span></a>
-	              <?php endif; ?>
-	            </li>
-	        </ul>
-	    </div>
-	</nav>
+									<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+										<a class="dropdown-item" href="logout.php">Logout</a>
+										<a class="dropdown-item" href="reset_password.php">Reset Password</a>
+									</div>
+								</li>
+
+							<?php else: ?>
+									<a class="nav-link text-right" href="login.php">Login <span class="sr-only">(current)</span></a>
+							<?php endif; ?>
+						</li>
+					</ul>
+        
+				
+				
+      </div>
+    </nav>
 
